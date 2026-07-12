fd67b7b1e60e636c87f59aace588f35066890a56
Shuiii <72098121+shuiiiiiii@users.noreply.github.com>
2026-07-12 20:50:13 +0530

    Create gallery.js

---
+++ server/public/scripts/gallery.js
@@ -0,0 +1,20 @@
+const homeBtn2 = document.getElementById("homeBtn");
+const abtBtn2 = document.getElementById("abtBtn");
+const contBtn2 = document.getElementById("contBtn");
+const servBtn2 = document.getElementById("servBtn");
+
+homeBtn2.addEventListener("click", () => {
+    window.location.href = "/";
+});
+
+abtBtn2.addEventListener("click", () => {
+    window.location.href = "/";
+});
+
+contBtn2.addEventListener("click", () => {
+    window.location.href = "/";
+});
+
+servBtn2.addEventListener("click", () => {
+    window.location.href = "/services";
+});
