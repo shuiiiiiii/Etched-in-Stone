382966c4e5ed6d02b540b49364d0e43d18df4630
Shuiii <72098121+shuiiiiiii@users.noreply.github.com>
2026-07-12 16:07:25 +0530

    Update indexRoutes.js
    - added service page route

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -6,8 +6,8 @@ router.get("/", (req, res) => {
     res.render("home");
 });
 
-router.get("/about", (req, res) => {
-    res.render("about");
+router.get("/services", (req, res) => {
+    res.render("services");
 });
 
 module.exports = router;
