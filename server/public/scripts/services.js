1e9535201325baddc4f4005aca6ad7be630368cd
Shuiii <72098121+shuiiiiiii@users.noreply.github.com>
2026-07-12 20:50:17 +0530

    Update services.js

--- server/public/scripts/services.js
+++ server/public/scripts/services.js
@@ -1,5 +1,9 @@
 const backToTopBtn2 = document.getElementById("backToTopBtn2");
 const homeBtn = document.getElementById("homeBtn");
+const gallBtn = document.getElementById("gallBtn");
+const abtBtn = document.getElementById("abtBtn");
+const contBtn = document.getElementById("contBtn");
+
 
 window.addEventListener('load', function() {
     const preloader = document.getElementById('preloader');
@@ -15,6 +19,22 @@ backToTopBtn2.addEventListener("click", () => {
   window.scrollTo({top:0, behavior: "smooth"});
 });
 
+// servBtn.addEventListener("click", () => {
+//     window.location.href = "/services";
+// });
+
 homeBtn.addEventListener("click", () => {
+    window.location.href = "/";
+});
+
+abtBtn.addEventListener("click", () => {
+    window.location.href = "/";
+});
+
+gallBtn.addEventListener("click", () => {
+    window.location.href = "/gallery";
+});
+
+contBtn.addEventListener("click", () => {
     window.location.href = "/";
 });
